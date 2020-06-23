dddc0d49d0f909834b1dc356f9823748291ec881
Forace Avant <themattavant@gmail.com>
2020-06-23 09:54:40 -0700

    Added a licence function in generateMarkdown.js that adds in a license section.

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 
-# README Generator
+# README_Generator
 
+
+## Description
+
+Creates a README.md file for a users repository.
+
+## Table of Content
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+
+## Installation
+
+To install dependencies, run: npm install
+
+## Usage
+
+To use this application correctly the user must run the command "node index.js" in the terminal and then answer the inquiries to populate a README.md file.
+
+## License
+    
+    This project has a MIT license
+
+## Contributing
+
+Please follow the Contributor Covenant when working with this repository.
+
+## Tests
+
+To run tests run the command: npm test
+
+## Questions
+
+If you have any questions on the repo contact ForaceAvant
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,20 @@
-
+function licenseLine(license){
+  if (license === "None"){
+    return `## License
+    
+    This project is not licensed.`
+  }
+  else{
+    return `## License
+    
+    This project has a ${license} license`
+  }
+}
 
 function generateMarkdown(data) {
   return `
 # ${data.title}
+
 
 ## Description
 
@@ -13,8 +25,30 @@ ${data.description}
 * [Installation](#installation)
 * [Usage](#usage)
 * [License](#license)
-* [Contributing](#contributers)
-* [Tests](#test)
+* [Contributing](#contributing)
+* [Tests](#tests)
+
+## Installation
+
+To install dependencies, run: ${data.installation}
+
+## Usage
+
+${data.usage}
+
+${licenseLine(data.license)}
+
+## Contributing
+
+${data.contributers}
+
+## Tests
+
+To run tests run the command: ${data.test}
+
+## Questions
+
+If you have any questions on the repo contact ${data.github}
 `;
 }
 
